fac6dc1103ca86d0acfddd94f09ae4cbcccfd631
horihiro <hori.kawa.shima.tool+github@gmail.com>
2022-01-23 14:46:42 +0900

    add option to specify base definition file

--- src/cli.rs
+++ src/cli.rs
@@ -7,20 +7,7 @@ use structopt::StructOpt;
   about = "A command line tool to generate Azure Custom Role Definition template"
 )]
 pub struct CommandLineArgs {
-  // /// Storage Account name
-  // #[structopt(short = "a", long = "account-name", required_unless = "")]
-  // pub account_name: String,
-
-  // /// Blob Container name
-  // #[structopt(short = "c", long = "container-name", required_unless = "")]
-  // pub container_name: String,
-
-  // /// Blob Path template
-  // #[structopt(short = "b", long = "blob-path-template", required_unless = "")]
-  // pub blob_path: String,
-
-  // / Blob type "AppendBlob", "BlockBlob" or "PageBlob"
-  //
-  // #[structopt(short = "t", long = "blob-type", default_value = "AppendBlob" )]
-  // pub blob_type: String,
+  /// Specify base role definition file path to append actions.
+  #[structopt(short = "a", long = "append-to")]
+  pub base_definition_filepath: String,
 }
